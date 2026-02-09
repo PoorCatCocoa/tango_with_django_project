f689f5745c4c3849146431cea19c12c807f5dbac
Guanjie <3075290W@student.gla.ac.uk>
2026-02-09 15:07:01 +0000

    Fixed URL mapping to include tango prefix

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says... hello world!")
+    # Build a dictionary and pass it to the template engine.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return to the rendered page
+    # The first parameter is the request, the second is the template path, and the third is a dictionary.
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.urls import include  # <--- Must import include
 
 urlpatterns = [
-    path('', include('rango.urls')), # Map the root path to rango
+    path('rango/', include('rango.urls')), # Map the root path to rango
     path('admin/', admin.site.urls),
 ]
