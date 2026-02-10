c8a6c98217d41f9ace0595f5f368eee6cf4805e2
Guanjie <3075290W@student.gla.ac.uk>
2026-02-10 01:35:48 +0000

    Completed Chapter 5: Created models and admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+# Customize how the Page is displayed in the background.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# Registration Model
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        # Corrected the display of plural forms in the background.
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    # Establish a foreign key relationship with Category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
