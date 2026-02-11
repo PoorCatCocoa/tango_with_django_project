cc4b62f4663ac4b1a1d61227553fdac34f8cba08
Guanjie <3075290W@student.gla.ac.uk>
2026-02-11 16:16:44 +0000

    Completed Chapter 7: Added slug field and category pages

--- rango/models.py
+++ rango/models.py
@@ -1,19 +1,25 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    # Add a slug field
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        # Automatically convert the name to a slug each time save.
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        # Corrected the display of plural forms in the background.
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 class Page(models.Model):
-    # Establish a foreign key relationship with Category
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page
 
 def index(request):
     # 1. Query the database: Retrieve the top 5 categories by number of likes
@@ -16,3 +16,25 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary
+    context_dict = {}
+
+    try:
+        # 1. Try to find the corresponding category based on slug.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # 2. If found, retrieve all pages belonging to this category.
+        pages = Page.objects.filter(category=category)
+
+        # 3. Put this data into a dictionary
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # 4. If not found, set the category to None.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Rendering template
+    return render(request, 'rango/category.html', context=context_dict)
